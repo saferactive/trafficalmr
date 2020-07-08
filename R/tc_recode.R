f544baa6de76a44d54a1465431b39508bf13f0c8
Robin Lovelace <rob00x@gmail.com>
2020-07-08 12:24:59 +0100

    Add recode function - wip

---
+++ R/tc_recode.R
@@ -0,0 +1,10 @@
+#' Recode vehicle types
+#'
+#'  This function was designed to simplify vehicle types from STAT19 data
+#'
+#' @examples
+#' x = stats19::vehicles_sample$vehicle_type
+#' tc_recode_vehicle_type(x)
+tc_recode_vehicle_type = function(x) {
+
+}
