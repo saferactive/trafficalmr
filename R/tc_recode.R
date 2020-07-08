58e8947d1ffc6f4167cb306ea896ce017edd958a
Robin Lovelace <rob00x@gmail.com>
2020-07-08 12:40:38 +0100

    Recode minibus

--- R/tc_recode.R
+++ R/tc_recode.R
@@ -6,13 +6,19 @@
 #' @examples
 #' (x = stats19::vehicles_sample$vehicle_type)
 #' tc_recode_vehicle_type(x)
+#' \dontrun{
+#' v = stats19::get_stats19(2018, "vehicles")
+#' v$vehicle_type_simple = tc_recode_vehicle_type(v$vehicle_type)
+#' table(v$vehicle_type)
+#' table(v$vehicle_type_simple)
+#' }
 tc_recode_vehicle_type = function(x) {
   vehicle_type_recoded = x
   vehicle_type_recoded[grepl("otorcyc", x)] = "Motorcycle"
   vehicle_type_recoded[grepl("7.5", x)] = "HGV"
   vehicle_type_recoded[grepl("Pedal", x)] = "Bicycle"
   vehicle_type_recoded[grepl("weight", x)] = "OtherGoods"
-  vehicle_type_recoded[grepl("coach", x)] = "Bus"
+  vehicle_type_recoded[grepl("coach|Minibus", x)] = "Bus"
   vehicle_type_recoded[grepl("Van", x)] = "OtherGoods"
   vehicle_type_recoded[grepl("Taxi", x)] = "Taxi"
   vehicle_type_recoded[is.na(vehicle_type_recoded)] = "OtherOrUnknown"
