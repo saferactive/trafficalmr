4083b17e4262308cea09b7480a09af1be41b386a
Robin Lovelace <rob00x@gmail.com>
2020-07-09 10:57:51 +0100

    Generalise tc_recode() functions

--- R/tc_recode.R
+++ R/tc_recode.R
@@ -1,11 +1,28 @@
-#' Recode vehicle types
+#' Recode character strings based on regular expressions
 #'
-#'  This function was designed to simplify vehicle types from STAT19 data
-#' @param x Vehicle types, e.g. from the vehicle_type column in STATS19 data
-#' @param pattern A named character vector with values representing new vehicle type names.
-#' Has the form `c("Car" = "Car", "Taxi/Private hire car" = "Taxi")`. See documentation
-#' for details.
+#' This function takes a character string and recodes it.
+#' Default recoding functions are available in functions such as
+#' `tc_recode_vehicle_type()` and `tc_recode_casualties`.
+#'
+#' It is based on the function [stringr::str_replace_all()].
+#'
+#' @param x Character string to recode
+#' @param pattern A named character vector with values representing new values.
+#' Has the form `c("Car long name" = "Car", "Taxi/Private hire car" = "Taxi")`.
+#' `Car long name` will be converted into `Car` in this case.
+#'
 #' @export
+#' @examples
+#' x = c("car long name", "bus", "a bike long")
+#' tc_recode(x, pattern = c("car.+" = "car"))
+#' tc_recode(x, pattern = c(".+bike*.+" = "bike"))
+#' tc_recode(x, pattern = c("car.+" = "car", ".+bike*.+" = "bike"))
+tc_recode = function(x, pattern) {
+  stringr::str_replace_all(x, pattern)
+}
+#' Recode vehicle types
+#' @export
+#' @rdname tc_recode
 #' @examples
 #' (x = stats19::vehicles_sample$vehicle_type)
 #' tc_recode_vehicle_type(x)
