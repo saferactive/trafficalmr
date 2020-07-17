cee155eeedb099f972fd655994ff0c59c34042b5
Robin Lovelace <rob00x@gmail.com>
2020-07-17 16:26:17 +0100

    Fix #25

--- R/upset.R
+++ R/upset.R
@@ -21,7 +21,16 @@
 #'      TRUE ~ "Other"
 #'     )
 #'   )
-#' crash_summary = tc_join_stats19(crashes_wf, casualties_wf2, vehicles_wf)
+#' table(vehicles_wf$vehicle_type)
+#' vehicles_wf2 = dplyr::mutate(
+#'   vehicles_wf,
+#'   vehicle_type_simple = dplyr::case_when(
+#'      vehicle_type == "Car" ~ "Car",
+#'      vehicle_type == "Cyclist" ~ "Pedal cycle",
+#'      TRUE ~ "Other"
+#'     )
+#'   )
+#' crash_summary = tc_join_stats19(crashes_wf, casualties_wf2, vehicles_wf2)
 #' tc_upset(crash_summary, casualty_type = c("Car", "Pedestrian", "Bicycle", "Other"))
 tc_upset = function(crash_summary,
                     casualty_type = c(
