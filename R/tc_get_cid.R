be805193d874902a7235d64c2126a248f0ff8eaa
Robin Lovelace <rob00x@gmail.com>
2020-09-16 18:52:26 +0100

    Update tc_get_cid() code

--- R/tc_get_cid.R
+++ R/tc_get_cid.R
@@ -1,16 +1,34 @@
-#' get cid data
+#' Get traffic calming data from the Cycling Infrastructure Database (CID)
 #'
-#' @param borough
+#' This function gets data from the CID.
+#' See  for more info.
+#'
+#'
+#' @param type What type of CID data?
+#' Options include "advanced_stop_line", "crossing", "cycle_lane_track" and
+# "restricted_route"
+#' @param tc_type What type of traffic calming intervention to get?
+#' Options are `NULL` (the default, meaning all traffic calming interventions)
+#' or "TRF_RAISED", "TRF_ENTRY", "TRF_CUSHI", "TRF_HUMP", "TRF_SINUSO",
+# "TRF_BARIER", "TRF_NAROW" or "TRF_CALM" to get a specific type.
+#' @param borough Which borough of London is of interest?
+#' `NULL` (all boroughs) is the default.
 #'
 #'
 #'
-#' @return
+#' @return An sf object representing traffic calming interventions
 #' @export
-#'
 #' @examples
-tc_get_cid = function(borough = NULL) {
+#' cid_traffic_calming = tc_get_cid()
+#' summary(cid_traffic_calming)
+#' cid_barriers = tc_get_cid(type = "TRF_BARIER")
+#' summary(cid_traffic_calming)
+tc_get_cid = function(type = "traffic_calming", tc_type = NULL, borough = NULL) {
   # download CID traffic calming data using the Cycle Infra Lnd package
-  traffic_calming = get_cid_points(type = "traffic_calming")
+
+  base_url = "https://cycling.data.tfl.gov.uk/CyclingInfrastructure/data/points/"
+  cid_url = paste0(base_url, type, ".json")
+  traffic_calming = sf::read_sf(cid_url)
 
   # convert certain columns to factors for analysis
   # NB BOROUGH needs doing separately as it has some NAs in, CLT_ACCESS not converted as 721 different values
@@ -25,15 +43,17 @@ tc_get_cid = function(borough = NULL) {
   # levels(f_traffic_calming$BOROUGH) # have 33 and no NA value
   #
   # # select traffic calming measures
+
+  traffic_calming[3:10] = lapply(sf::st_drop_geometry(traffic_calming[3:10]), as.logical)
   if(!is.null(borough)) {
-    traffic_calming = traffic_calming %>%
-      dplyr::filter(BOROUGH == borough)
+    traffic_calming = traffic_calming[traffic_calming[["BOROUGH"]] == borough, ]
   }
 
-
   # select traffic calming barriers
-  tc_barriers = traffic_calming %>%
-    dplyr::filter(TRF_BARIER == "TRUE")
+  if(!is.null(type)) {
+    traffic_calming = traffic_calming[traffic_calming[[type]], ]
+  }
+  traffic_calming
 }
 
 # code from: https://github.com/PublicHealthDataGeek/CycleInfraLnd/blob/master/R/get_cid.R
@@ -42,19 +62,19 @@ tc_get_cid = function(borough = NULL) {
 # get_cid_lines(type = "crossing")
 # get_cid_lines(type = "cycle_lane_track")
 # get_cid_lines(type = "restricted_route")
-get_cid_lines = function(type) {
-  base_url = "https://cycling.data.tfl.gov.uk/CyclingInfrastructure/data/lines/"
-  cid_url = paste0(base_url, type, ".json")
-  sf::read_sf(cid_url)
-}
+# get_cid_lines = function(type) {
+#   base_url = "https://cycling.data.tfl.gov.uk/CyclingInfrastructure/data/lines/"
+#   cid_url = paste0(base_url, type, ".json")
+#   sf::read_sf(cid_url)
+# }
 
 # get_cid_points(type = "signal")
 # get_cid_points(type = "cycle_parking")
 # get_cid_points(type = "restricted_point")
 # get_cid_points(type = "signage")
 # get_cid_points(type = "traffic_calming")
-get_cid_points = function(type) {
-  base_url = "https://cycling.data.tfl.gov.uk/CyclingInfrastructure/data/points/"
-  cid_url = paste0(base_url, type, ".json")
-  sf::read_sf(cid_url)
-}
+# get_cid_points = function(type) {
+#   base_url = "https://cycling.data.tfl.gov.uk/CyclingInfrastructure/data/points/"
+#   cid_url = paste0(base_url, type, ".json")
+#   sf::read_sf(cid_url)
+# }
