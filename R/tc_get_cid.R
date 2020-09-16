1b1d4d3b0765f39fb255d0faf934c29df070174d
Robin Lovelace <rob00x@gmail.com>
2020-09-16 18:19:54 +0100

    Add cid code

--- R/tc_get_cid.R
+++ R/tc_get_cid.R
@@ -35,3 +35,26 @@ tc_get_cid = function(borough = NULL) {
   tc_barriers = traffic_calming %>%
     dplyr::filter(TRF_BARIER == "TRUE")
 }
+
+# code from: https://github.com/PublicHealthDataGeek/CycleInfraLnd/blob/master/R/get_cid.R
+#
+# get_cid_lines(type = "advanced_stop_line")
+# get_cid_lines(type = "crossing")
+# get_cid_lines(type = "cycle_lane_track")
+# get_cid_lines(type = "restricted_route")
+get_cid_lines = function(type) {
+  base_url = "https://cycling.data.tfl.gov.uk/CyclingInfrastructure/data/lines/"
+  cid_url = paste0(base_url, type, ".json")
+  sf::read_sf(cid_url)
+}
+
+# get_cid_points(type = "signal")
+# get_cid_points(type = "cycle_parking")
+# get_cid_points(type = "restricted_point")
+# get_cid_points(type = "signage")
+# get_cid_points(type = "traffic_calming")
+get_cid_points = function(type) {
+  base_url = "https://cycling.data.tfl.gov.uk/CyclingInfrastructure/data/points/"
+  cid_url = paste0(base_url, type, ".json")
+  sf::read_sf(cid_url)
+}
